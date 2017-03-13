34d093bf40d8373be59caa98baabe9615a6b0e26
erpalma <palmarini@unive.it>
2017-03-13 12:35:27 +0100

    ticked version number 0.9.5

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='mignis',
     py_modules=['mignis', 'ipaddr_ext'],
-    version='0.9.3',
+    version='0.9.5',
     license='MIT',
     description='Mignis is a semantic based tool for firewall configuration',
+    long_description=readme(),
     url='https://github.com/segroup/Mignis',
     keywords=['iptables', 'firewall', 'semantic firewall configuration', 'netfilter'],
     install_requires=['ipaddr'],
